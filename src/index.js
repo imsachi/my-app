db185c1b770d30da9fb37c571f6159a5943e084d
imsachi <63872913+imsachi@users.noreply.github.com>
2020-08-13 15:50:56 +0530

    my second commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-
+let msg=undefined;
 class  Myapp extends React.Component {
-
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+handleClick(e){
+ msg = 'im clicked';
+ console.log(msg);
+}
   render() { 
     return ( 
       <div>
@@ -14,6 +21,8 @@ class  Myapp extends React.Component {
         </nav>
         <h1>welcome to my testing website</h1>
         <p>thank u for visiting my website this is website i created for testing</p>
+        {msg}
+        <button onClick={this.handleClick}>click me</button>
       </div>
      );
   }
